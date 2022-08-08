20081df4cf975428885de54b28256448a1b71891
SuebSaifi <sueb18-cs@danskar.org>
2022-08-08 19:03:42 +0530

    Adding Search By Rating

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -10,6 +10,8 @@ class SearchsController < ApplicationController
             @searchproducts= @searchproducts.where(category_id: filter_params[:category_id]) if filter_params[:category_id].present?
             @searchproducts= @searchproducts.where(price: (filter_params[:min]..filter_params[:max])) if (filter_params[:min] || filter_params[:max]).present?  
             @searchproducts=@searchproducts.where(color: filter_params[:color]) if (filter_params[:color]).present?
+            debugger
+            @searchproducts= @searchproducts.joins(:ratings).group("rating").having("avg(rating) <=?", filter_params[:rating])if (filter_params[:rating]).present?
         end
         @searchproducts = @searchproducts.paginate(page: params[:page], per_page: 5)
         render 'index'
@@ -19,6 +21,6 @@ class SearchsController < ApplicationController
     
     def filter_params
 
-        params.require(:filter).permit(:brand_id, :category_id, :min, :max, :query,:color)
+        params.require(:filter).permit(:brand_id, :category_id, :min, :max, :query,:color,:rating)
     end
 end
