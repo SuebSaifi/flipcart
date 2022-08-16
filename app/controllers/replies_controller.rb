2c1f843707b004815ae92890ccd8f26dee02bf14
SuebSaifi <sueb18-cs@danskar.org>
2022-08-16 18:58:59 +0530

    multilevel reply

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,10 +1,8 @@
 class RepliesController < ApplicationController
-    before_action :set_product
-    before_action :set_review,only:[:new]
-
-
+    before_action :set_product,:set_reply,:set_review,only:[:new]
     def new 
-        @reply = current_user.replies.build
+        @review=Review.find(params[:review_id])
+        @reply = @review.replies.new(parent_id: params[:parent_id])
     end
     def create
     @review=Review.find(params[:review_id])
@@ -13,15 +11,14 @@ class RepliesController < ApplicationController
             redirect_to @product
         end
     end
-      
-  def destroy
-    @reply = Reply.find(params[:id])
-    @reply.destroy
-    respond_to do |format|
-        format.html { redirect_to @product    }
-        format.json { head :no_content }
-        format.js   { render :layout => false }
-     end
+    def destroy
+        @reply = Reply.find(params[:id])
+        @reply.destroy
+        respond_to do |format|
+            format.html { redirect_to @product    }
+            format.json { head :no_content }
+            format.js   { render :layout => false }
+        end
   end
     private
     def set_product
@@ -30,7 +27,10 @@ class RepliesController < ApplicationController
     def set_review
         @review=Review.find(params[:review_id])
     end
+    def set_reply
+        @reply=Reply.find(params[:reply_id])
+    end
     def reply_params
-        params.require(:reply).permit(:user_id,:review_id,:reply)
+        params.require(:reply).permit(:user_id,:review_id,:reply,:parent_id)
     end
 end
